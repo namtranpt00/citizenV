c1c9c3bfdc5c20396e9dbbb23d6adcfad839e8ad
nam_tnp <nam_tnp@flinters.vn>
2021-11-23 22:51:02 +0700

    create

---
+++ app-frontend/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+
+    var res = fetch('http://127.0.0.1:8000/api/home', {
+        mode: 'no-cors',
+        method: "get",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    console.log(res)
+
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload. helu
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+
+    );
+
+}
+
+export default App;
